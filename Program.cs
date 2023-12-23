f14168518bee1c4bd10ceddb7d14ce0f4e828574
tatiana <lazarevatatiana18@gmail.com>
2023-12-23 22:39:06 +0300

    added first task

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 ﻿// Два правых слэша означает однострочный комментарий
 
-//  просто текст (быстрое комментирование) ntrl+ правый слэш
+//  просто текст (быстрое комментирование) cntrl+ правый слэш
 
 // тип_данных имя_переменной = значение;
 
-int number = 9; //number = 9
-int result = number * number; //результат 81
+// using System.IO.Compression;
 
- 
+// int number = 9; //number = 9
+// int result = number * number; //результат 81
+// Console.Write("Введите число: ");
+// int input = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine(input * input);
+
+ Console.Write("Введите первое число: ");
+ int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+ Console.Write("Введите второе число: ");
+ int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+ if(firstNumber == secondNumber * secondNumber)
+ {
+    Console.WriteLine("Да");
+ }
+else {
+    Console.WriteLine("Нет");
+}
